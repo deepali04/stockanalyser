44cd1ae2a8311d5c3c3b43b81ecedc1e6e43d766
Sai Shruthi Sistla <saishruthi.sistla@gmail.com>
2024-05-09 22:51:57 -0400

    Update test.py

--- stockanalyser/test.py
+++ stockanalyser/test.py
@@ -1,4 +1,4 @@
-from stocks_analyzer import get_stock_data, plot_compare_stocks
+from stocks_analyzer import get_stock_data, plot_compare_stocks, analyze
 import datetime
 
 def main():
@@ -11,6 +11,10 @@ def main():
         tickers = [ticker.upper() for ticker in tickers]
         stock_data = get_stock_data(tickers, start, end)
         plot_compare_stocks(stock_data)
+        for ticket in tickers:
+            print(ticket.upper())
+            stock_data=get_stock_data([ticket],start,end)
+            print(analyze(stock_data,ticket))
 
     except ValueError as e:
         print(f"Validation error: {e}")
@@ -21,31 +25,7 @@ def main():
 if __name__ == "__main__":
     main()
 
-# import unittest
-# from stocks_analyzer import get_stock_data, plot_compare_stocks
-# from utils import validate_tickers
 
-# class TestStockAnalysisPackage(unittest.TestCase):
-    
-#     def test_validate_tickers(self):
-#         """Test the validate_tickers function with both valid and invalid tickers."""
-#         input_tickers = ['AAPL', 'GOOGL', 'XXXXX']
-#         expected_output = ['XXXXX']  # Assuming 'XXXXX' is an invalid ticker
-#         result = validate_tickers(input_tickers)
-#         self.assertEqual(result, expected_output)
 
-#     def test_get_stock_data(self):
-#         """Test the get_stock_data function to ensure it returns data correctly."""
-#         tickers = ['AAPL', 'MSFT']
-#         start = '2022-01-01'
-#         end = '2022-12-31'
-#         data = get_stock_data(tickers, start, end)
-#         # Check if data for each ticker is not empty
-#         for ticker, df in data.items():
-#             self.assertFalse(df.empty, f"Data for {ticker} is empty")
 
-#     # Add more tests for other functions like analyze, plot functions, etc.
-    
-# if __name__ == '__main__':
-#     unittest.main()
 
